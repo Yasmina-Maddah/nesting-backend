b018dcf0e6a8539ad052067b4e2b75265e1fc9e6
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-14 20:45:53 +0200

    Added the Routes of Signup and Login Apis

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->get('/user', [AuthController::class, 'getUser']);
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
