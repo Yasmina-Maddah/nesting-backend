5d77ac1ec52cfb06e47472b6bdf64ad03f40f01b
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-14 20:25:07 +0200

    Added the Skill Model

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Skill extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'skill_name',
+        'description',
+    ];
+
+    // Relationships
+    public function childSkills()
+    {
+        return $this->hasMany(ChildSkill::class, 'skill_id');
+    }
+
+    public function aiVisualizations()
+    {
+        return $this->hasMany(AIVisualization::class, 'skill_id');
+    }
 }
