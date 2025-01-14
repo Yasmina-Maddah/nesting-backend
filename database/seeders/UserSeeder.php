aa62df4e2ff6c6991cb3fabf4674c9a36c7ddd97
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-14 20:30:49 +0200

    Created Users Seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,32 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        User::create([
+            'username' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'user_type' => 'admin',
+        ]);
+
+        User::create([
+            'username' => 'parent1',
+            'email' => 'parent1@example.com',
+            'password' => bcrypt('password'),
+            'user_type' => 'parent',
+        ]);
+
+        User::create([
+            'username' => 'parent2',
+            'email' => 'parent2@example.com',
+            'password' => bcrypt('password'),
+            'user_type' => 'parent',
+        ]);
     }
 }
