05759d43c8734a12b835562d96668ba2abf4603d
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-14 20:31:53 +0200

    Created Skill Seeders

--- database/seeders/SkillSeeder.php
+++ database/seeders/SkillSeeder.php
@@ -2,16 +2,15 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Skill;
 
 class SkillSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        Skill::create(['skill_name' => 'Math', 'description' => 'Basic arithmetic and problem-solving skills.']);
+        Skill::create(['skill_name' => 'Reading', 'description' => 'Enhancing comprehension and vocabulary.']);
+        Skill::create(['skill_name' => 'Creativity', 'description' => 'Developing artistic and imaginative skills.']);
     }
 }
