6df28f301fd696c4501aa604dc412df35295c493
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-14 20:26:05 +0200

    Added the Progress Report Model

--- app/Models/ProgressReport.php
+++ app/Models/ProgressReport.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProgressReport extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'child_skill_id',
+        'progress_entry',
+    ];
+
+    // Relationships
+    public function childSkill()
+    {
+        return $this->belongsTo(ChildSkill::class, 'child_skill_id');
+    }
 }
