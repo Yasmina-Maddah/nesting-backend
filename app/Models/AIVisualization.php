8167c1a3aad0a6482a54b67502094a263da21227
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-14 20:26:41 +0200

    Added the Ai Model

--- app/Models/AIVisualization.php
+++ app/Models/AIVisualization.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AIVisualization extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'parent_id',
+        'skill_id',
+        'story_input',
+        'visualization',
+    ];
+
+    // Relationships
+    public function parent()
+    {
+        return $this->belongsTo(User::class, 'parent_id');
+    }
+
+    public function skill()
+    {
+        return $this->belongsTo(Skill::class, 'skill_id');
+    }
 }
