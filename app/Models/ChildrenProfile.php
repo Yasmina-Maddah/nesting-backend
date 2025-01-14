49e60f050431466c61d17ff91b6165fcfa828bfe
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-14 20:24:32 +0200

    Added the Children Profile Model

--- app/Models/ChildrenProfile.php
+++ app/Models/ChildrenProfile.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ChildrenProfile extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'parent_id',
+        'name',
+        'profile_photo',
+        'cover_photo',
+        'age',
+    ];
+
+    // Relationships
+    public function parent()
+    {
+        return $this->belongsTo(User::class, 'parent_id');
+    }
+
+    public function skills()
+    {
+        return $this->hasMany(ChildSkill::class, 'child_id');
+    }
 }
