f4b0ec12dafc6e4368dd4c7bb125bbcbe331555b
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-14 20:31:28 +0200

    Created Children Profile Seeders

--- database/seeders/ChildrenProfileSeeder.php
+++ database/seeders/ChildrenProfileSeeder.php
@@ -2,16 +2,27 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\ChildrenProfile;
 
 class ChildrenProfileSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        ChildrenProfile::create([
+            'parent_id' => 2, // Parent1 ID
+            'name' => 'John Doe',
+            'age' => 7,
+            'profile_photo' => 'john.jpg',
+            'cover_photo' => 'john_cover.jpg',
+        ]);
+
+        ChildrenProfile::create([
+            'parent_id' => 3, // Parent2 ID
+            'name' => 'Jane Smith',
+            'age' => 9,
+            'profile_photo' => 'jane.jpg',
+            'cover_photo' => 'jane_cover.jpg',
+        ]);
     }
 }
