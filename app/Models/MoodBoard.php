574d1723be38119e4600f60f71e37976321149b6
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-14 22:24:38 +0200

    Edited the Model of the Mood Board

--- app/Models/MoodBoard.php
+++ app/Models/MoodBoard.php
@@ -5,33 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ChildrenProfile extends Model
+class MoodBoard extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'parent_id',
-        'name',
-        'profile_photo',
-        'cover_photo',
-        'date_of_birth',
-        'hobbies',
-        'dream_career',
+        'child_profile_id',
+        'image_path',
+        'description',
     ];
 
-    // Relationships
-    public function parent()
+    // Relationship with ChildrenProfile
+    public function childProfile()
     {
-        return $this->belongsTo(User::class, 'parent_id');
-    }
-
-    public function skills()
-    {
-        return $this->hasMany(ChildSkill::class, 'child_id');
-    }
-
-    public function moodBoards()
-    {
-        return $this->hasMany(MoodBoard::class, 'child_profile_id');
+        return $this->belongsTo(ChildrenProfile::class, 'child_profile_id');
     }
 }
