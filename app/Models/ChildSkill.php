16db2d7309fd659450e8681291d692826327e371
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-14 20:25:32 +0200

    Added the Child Skill Model

--- app/Models/ChildSkill.php
+++ app/Models/ChildSkill.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ChildSkill extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'child_id',
+        'skill_id',
+        'progress',
+    ];
+
+    // Relationships
+    public function child()
+    {
+        return $this->belongsTo(ChildrenProfile::class, 'child_id');
+    }
+
+    public function skill()
+    {
+        return $this->belongsTo(Skill::class, 'skill_id');
+    }
+
+    public function progressReports()
+    {
+        return $this->hasMany(ProgressReport::class, 'child_skill_id');
+    }
 }
