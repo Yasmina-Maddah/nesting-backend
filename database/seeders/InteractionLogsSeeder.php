84050e7026b4fa34a41147335ef7f1505831711c
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-15 21:14:06 +0200

    Added Seeders to IntegrationLog

--- database/seeders/InteractionLogsSeeder.php
+++ database/seeders/InteractionLogsSeeder.php
@@ -2,16 +2,18 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\InteractionLog;
 
 class InteractionLogsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        InteractionLog::create([
+            'child_id' => 1,
+            'activity_id' => 1,
+            'interaction_time' => now(),
+            'notes' => 'Child completed the puzzle within 30 minutes.',
+        ]);
     }
 }
