527eadfa0dd312c7052c6501c31d4182f12a65c6
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-15 21:11:57 +0200

    Edited the Report seeders

--- database/seeders/ProgressReportSeeder.php
+++ database/seeders/ProgressReportSeeder.php
@@ -5,12 +5,14 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\ProgressReport;
 
-class ProgressReportSeeder extends Seeder
+class ProgressReportsSeeder extends Seeder
 {
     public function run()
     {
-        ProgressReport::create(['child_skill_id' => 1, 'progress_entry' => 50]); // John - Math Progress
-        ProgressReport::create(['child_skill_id' => 2, 'progress_entry' => 70]); // John - Reading Progress
-        ProgressReport::create(['child_skill_id' => 3, 'progress_entry' => 40]); // Jane - Creativity Progress
+        ProgressReport::create([
+            'child_skill_id' => 1,
+            'progress_entry' => 75,
+            'details' => json_encode(['Milestone' => 'Solved advanced puzzles']),
+        ]);
     }
 }
