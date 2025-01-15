ee8ee73ad65f358ae18bd3d2bbca148611947375
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-15 20:45:57 +0200

    Added the InteractionLog Model

--- app/Models/InteractionLog.php
+++ app/Models/InteractionLog.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class InteractionLog extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['child_id', 'activity_id', 'interaction_time', 'notes'];
+
+    public function child()
+    {
+        return $this->belongsTo(ChildrenProfile::class, 'child_id');
+    }
+
+    public function activity()
+    {
+        return $this->belongsTo(Activity::class, 'activity_id');
+    }
 }
