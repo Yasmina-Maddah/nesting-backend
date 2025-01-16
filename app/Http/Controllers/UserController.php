accfd75fc9420561b8319142e735b6a638458e05
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-16 02:48:10 +0200

    Added the code of the controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    public function getParentDetails(Request $request)
+    {
+        $user = Auth::user(); // Get the authenticated user
+        
+        if ($user && $user->user_type === 'parent') {
+            return response()->json([
+                'username' => $user->username,
+                'email' => $user->email,
+            ]);
+        }
+
+        return response()->json(['error' => 'User not found or not a parent'], 404);
+    }
 }
+
