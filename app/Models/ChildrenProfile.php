aadd323689ad2efcc223318d586943bb3328ace1
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-19 19:33:36 +0200

    Edited the Children profile model

--- app/Models/ChildrenProfile.php
+++ app/Models/ChildrenProfile.php
@@ -5,41 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ChildrenProfile extends Model
+class ChildProfile extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['parent_id', 'name', 'profile_photo', 'cover_photo', 'date_of_birth', 'hobbies', 'dream_career', 'archived'];
-
-    protected $casts = [
-        'hobbies' => 'array',
-        'archived' => 'boolean',
+    protected $fillable = [
+        'user_id',
+        'name',
+        'date_of_birth',
+        'hobbies',
+        'dream_job',
     ];
 
     public function parent()
     {
-        return $this->belongsTo(User::class, 'parent_id');
-    }
-
-    public function moodBoards()
-{
-    return $this->hasMany(MoodBoard::class);
-}
-
-
-    public function childSkills()
-    {
-        return $this->hasMany(ChildSkill::class, 'child_id');
-    }
-
-    public function interactionLogs()
-    {
-        return $this->hasMany(InteractionLog::class, 'child_id');
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function skills()
     {
-    return $this->belongsToMany(Skill::class, 'child_skills', 'child_id', 'skill_id')
-                ->withPivot('id', 'progress');
+        return $this->hasMany(ChildSkill::class, 'child_id');
     }
 }
