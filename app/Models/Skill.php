9629a956383bdb63968bff72a595b59b625a2b1d
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-19 19:36:13 +0200

    Edited the Skill table

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -9,25 +9,13 @@ class Skill extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['skill_name', 'description', 'image_path', 'parent_skill_id'];
-
-    public function parentSkill()
-    {
-        return $this->belongsTo(Skill::class, 'parent_skill_id');
-    }
+    protected $fillable = [
+        'skill_name',
+        'description',
+    ];
 
     public function childSkills()
     {
         return $this->hasMany(ChildSkill::class, 'skill_id');
-    }
-
-    public function aiVisualizations()
-    {
-        return $this->hasMany(AiVisualization::class, 'skill_id');
-    }
-
-    public function progressReports()
-    {
-    return $this->hasOne(ProgressReport::class, 'child_skill_id');
     }
 }
