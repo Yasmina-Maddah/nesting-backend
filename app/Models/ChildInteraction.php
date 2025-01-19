4cdb3718cb4275e2466fbd8a08d46cc95409ba31
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-19 19:41:21 +0200

    Added the Child Interaction Model

--- app/Models/ChildInteraction.php
+++ app/Models/ChildInteraction.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ChildInteraction extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'child_id',
+        'visualization_id',
+        'response',
+        'is_correct',
+    ];
+
+    public function child()
+    {
+        return $this->belongsTo(ChildProfile::class, 'child_id');
+    }
+
+    public function visualization()
+    {
+        return $this->belongsTo(AIVisualization::class, 'visualization_id');
+    }
 }
