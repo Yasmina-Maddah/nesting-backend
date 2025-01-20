7ea715d75e59e3df2294e29f87e6d62e19451e1b
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-20 13:37:00 +0200

    Added IsAdmin middleware

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -4,17 +4,16 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class IsAdmin
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if (auth()->user() && auth()->user()->user_type === 'admin') {
+            return $next($request);
+        }
+
+        return redirect('/'); // Redirect non-admin users
     }
 }
+
