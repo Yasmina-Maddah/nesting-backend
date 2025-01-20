d7bc14cd7182657b6a7b745290434d1e8d94109e
Yasmina Maddah <maddahyasmina@gmail.com>
2025-01-20 19:55:23 +0200

    Removed the comments of the Artictles Controller

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    // Parent: Display searchable articles
     public function index(Request $request)
     {
         $query = $request->input('query');
@@ -20,13 +19,11 @@ class ArticleController extends Controller
         return view('articles.index', compact('articles'));
     }
 
-    // Admin: Show form to create article
     public function create()
     {
         return view('articles.create');
     }
 
-    // Admin: Store a new article
     public function store(Request $request)
     {
         $request->validate([
@@ -39,7 +36,6 @@ class ArticleController extends Controller
         return redirect()->route('articles.index')->with('success', 'Article created successfully.');
     }
 
-    // Admin: Show form to edit article
     public function edit($id)
     {
         $article = Article::findOrFail($id);
@@ -47,7 +43,6 @@ class ArticleController extends Controller
         return view('articles.edit', compact('article'));
     }
 
-    // Admin: Update an article
     public function update(Request $request, $id)
     {
         $request->validate([
@@ -61,7 +56,6 @@ class ArticleController extends Controller
         return redirect()->route('articles.index')->with('success', 'Article updated successfully.');
     }
 
-    // Admin: Delete an article
     public function destroy($id)
     {
         $article = Article::findOrFail($id);
